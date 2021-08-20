6698b1fb3253a9346a58f8c0f26f61b28b0b7fca
gpreye <46998526+gpreye@users.noreply.github.com>
2021-08-20 06:03:15 -1100

    Initial Bitcoin Website

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Show Menu
+function navSlide() {
+    let burger = document.querySelector(".burger");
+    let navLinks = document.querySelector(".nav-links");
+    burger.classList.toggle("show");
+    navLinks.classList.toggle("show");
+}
